6dd8eac37b42c359064e5e224817fa91d2eda308
Endrina Toledo <etoledo@wau.com>
2023-06-09 10:29:20 -0400

    se elimina expo

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      
+      <Text>
+        App
+      </Text>
+      </View>
+  )
+}
+
+export default App
